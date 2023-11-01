167ad231c4c1fed8596913d38bd4326f236d68ac
Janvi sahu <sahujanvi317@gmail.com>
2023-11-01 19:56:42 +0530

    readable and maintainable updated starpattern code

--- practicals/starpattern.php
+++ practicals/starpattern.php
@@ -1,14 +1,18 @@
 <?php
-$rows = 5; // Change the number of rows as per your requirement
+// This is a PHP program that prints an inverted left triangle pattern of stars.
 
-for ($i = 1; $i <= $rows; $i++) {
-    for ($j = 1; $j <= $i - 1; $j++) {
-        echo " ";
+// Define the number of rows for the inverted left triangle.
+$rows = 5; // You can change this to the number of rows you want.
+
+// Loop through each row.
+for ($i = $rows; $i >= 1; $i--) {
+    // Loop to print the stars for the current row.
+    for ($j = 1; $j <= $i; $j++) {
+        echo "*";
     }
-    for ($k = $i; $k <= $rows; $k++) {
-        echo "* ";
-    }
-    echo "<br>";
+
+    // Move to the next line for the next row.
+    echo "\n";
 }
 ?>
 
