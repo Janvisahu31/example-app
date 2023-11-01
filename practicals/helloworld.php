41d0258fee37e7fb44ef1c3ae515a7c4429c0375
Janvi sahu <sahujanvi317@gmail.com>
2023-11-01 19:41:19 +0530

    readable and maintainable updated hello world  code

--- practicals/helloworld.php
+++ practicals/helloworld.php
@@ -1,4 +1,10 @@
 <?php
-echo "Hello, World!";
+// This is a PHP program that prints "Hello, World!" to the screen.
+
+// Define a variable to hold the message.
+$message = "Hello, World!";
+
+// Use the echo statement to output the message to the screen.
+echo $message;
 ?>
 
