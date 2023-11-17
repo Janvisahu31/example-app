a2a60c2eb5d89ae3894dcf2cb52ca6f4b75999f0
Janvi sahu <sahujanvi317@gmail.com>
2023-11-17 15:34:19 +0530

    Add routes for veggies

---
+++ resources/views/veggies.blade.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<h2>
+vegetables</h2>
+<table>
+<tr>
+<th>vegetable name</th>
+<th>price</th>
+</tr>
+<th>Baigan</th>
+<th>20</th>
+<th>Bhindi</th>
+<th>25</th>
+<th>Aaloo</th>
+<th>30</th>
+<th>Gobhi</th>
+<th>15</th>
